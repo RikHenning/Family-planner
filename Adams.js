7a1e05b51f2810b473fd4bd6e7e00ae88600aee0
RikHenning <144657312+RikHenning@users.noreply.github.com>
2023-10-27 08:39:49 +0200

    27-10-2023
    See you tomorrow

--- Adams.js
+++ Adams.js
@@ -1,3 +1,21 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+
+localStorage.setItem(['Description': 'test one'], ['Due Date': 'dd/mm/yyyy'], ['Priority': 'Medium'], ['Category': 'Personal']);
+
+function createNewTask(event) {
+  const description = event.target.elements.inputDescription.value;
+  const dueDate = event.target.elements.inputDueDate.value;
+  const priority = event.target.input[type:'radio']:checked^label.value;
+  const category = event.target.category.value;
+
+  localStorage.setItem(description, dueDate, priority, category);
+
+  console.log( Object.entries(localStorage) );
+}
+
+newTaskForm.addEventListener("submit", (event) => {
+  createNewTask(event);
+  }
