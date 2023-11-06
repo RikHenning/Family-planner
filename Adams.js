a14773894a8e296b59c269aaaa95e88b464bacc6
RikHenning <144657312+RikHenning@users.noreply.github.com>
2023-11-06 08:56:28 +0100

    6-11-2023
    Been fighting with the function displayTaskList. Not sure if you could give me a little hint as how to procede. It seems to not be able to understand taskId or how to read a task after it got one.

--- Adams.js
+++ Adams.js
@@ -1,6 +1,6 @@
 
 localStorage.clear();
-localStorage.setItem('tasks', JSON.stringify([{'Task Name': 'Task Name', 'Description': 'test one', 'Due Date': 'dd/mm/yyyy', 'Priority': 'Medium', 'Category': 'Personal'}]));
+localStorage.setItem('tasks', JSON.stringify([{'taskId': 0, 'taskName': 'Default',  'description': 'Default', 'dueDate': 'Default','priority': 'Default', 'category': 'Default'}]));
 
 const newTaskForm = document.getElementById('newTaskForm');
 
@@ -9,14 +9,12 @@ const newTaskForm = document.getElementById('newTaskForm');
   return JSON.parse(localStorage.getItem('tasks'));
 }
 
-// localStorage.clear();
-
 function pushTaskToLocalStorage(task) {
 
   const existingTasks = getTasksFromLocalStorage();
 
   existingTasks.push(task);
-  console.log(existingTasks);
+  // console.log(existingTasks);
   localStorage.setItem('tasks', JSON.stringify(existingTasks));
   console.log(getTasksFromLocalStorage());
 
@@ -28,41 +26,19 @@ function createNewTask(event) {
   const taskName = event.target.inputTask.value;
   const description = event.target.elements.inputDescription.value;
   const dueDate = event.target.elements.inputDueDate.value;
-  let prioChecked 
+  let priority = '';
     if (event.target.flexRadioDefault1.checked === true) {
-      prioChecked = event.target.flexRadioDefault1.checked;
+      priority = 'Low'
     }
     else if (event.target.flexRadioDefault2.checked === true) {
-      prioChecked  = event.target.flexRadioDefault2.checked;
+     priority = 'Medium'
     }
     else if (event.target.flexRadioDefault3.checked === true) {
-      prioChecked  = event.target.flexRadioDefault3.checked; 
+      priority = 'High' 
     } 
-  switch (prioChecked) {
-    case event.target.flexRadioDefault1.checked:
-      prioName = 'Low'
-      break;
-    case event.target.flexRadioDefault2.checked:
-      prioName = 'Medium'
-      break;
-    case event.target.flexRadioDefault3.checked:
-      prioName = 'High'
-      break;
-  }
-  // let prioName 
-  //   if (prioChecked === event.target.flexRadioDefault1.checked) {
-  //     return 'Low';
-  //   }
-  //   else if (prioChecked === event.target.flexRadioDefault2.checked) {
-  //     return 'Medium';
-  //   }
-  //   else if (prioChecked === event.target.flexRadioDefault3.checked) {
-  //     return 'High';
-  //   }
-  const priority = prioName
   const category = event.target.category.value;
   const taskNumber = Math.floor(Math.random() * 100);
-  const task = {'Task Name': taskName,  'Description': description, 'Due Date': dueDate,'Priority': priority, 'Category': category} 
+  const task = {'taskId': Date.now(), 'taskName': taskName,  'description': description, 'dueDate': dueDate,'priority': priority, 'category': category} 
   pushTaskToLocalStorage(task)
   
 }
@@ -71,22 +47,54 @@ newTaskForm.addEventListener("submit", (event) => {
   createNewTask(event);
   });
 
+function getTaskById(taskId) {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  taskResult = tasks.filter(task => task.taskId === taskId)
+}
+
+function createTaskListRow(task, taskId) {
+  const row = document.createElement("tr");
+
+  const idCell = document.createElement("th");
+  idCell.textContent = task.taskId;
+  const taskName = document.createElement("td");
+  taskName.textContent = task.taskName;
+  const description = document.createElement("td");
+  description.textContent = task.description;
+  const dueDate = document.createElement("td");
+  dueDate.textContent = task.dueDate; 
+  const priority = document.createElement("td");
+  priority.textContent = task.priority;
+  const category = document.createElement("td");
+  category.textContent = task.category;
+  const deleteButton = document.createElement("button");
+
+  const editButton = document.createElement("button");
+}
+
+function displayTaskList() {
+
+  taskListBody.innerHTML = "";
+
+  const taskId = Math.floor(Math.random()*2699256952556);
+  const task = getTaskById(taskId);
+
+    task.forEach((task, taskId) => {
+      const taskRow = createTaskListRow(task, taskId);
+      taskListBody.appendChild(taskRow)
+    });  
+}
+
+displayTaskList();
+
+
 // $('#myModal').on('shown.bs.modal', function () {
 //   $('#myInput').trigger('focus')
-// }) I want to get the Modal working so I can place the form of making a new task inside it.
- // This code appaerntly I need to make the Modal work but it throws an error I don't understand 
-// how this code is build up in order for me to have a stab at fixing it.
+// })
 
- // getTasksFromLocalStorage()
 
- // console.log(getTasksFromLocalStorage());
-
-function taskListReveal() { //create for each loop. get data from local storage. print data on list in HTML.
-  const task
-  const taskName 
-  const description
-  const dueDate
-  const priority
-  const category
-}
+// 1) explain task list in html. (in bootstrap is a table component)
+// 2) Delete button
+// 3) edit button
+// 4) make the modal working.
 
