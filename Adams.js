f4053c551b41bdb17c3a7c81a56edecc32dee46b
RikHenning <144657312+RikHenning@users.noreply.github.com>
2023-11-10 09:06:02 +0100

    10-11-2023
    Thanks for the code! I think I have edited it and I started to improve upon the edit button But you might have other ideas. Can't wait for tomorrow.

--- Adams.js
+++ Adams.js
@@ -3,49 +3,22 @@ localStorage.clear();
 localStorage.setItem('tasks', JSON.stringify([{'taskId': 0, 'taskName': 'Default',  'description': 'Default', 'dueDate': 'Default','priority': 'Default', 'category': 'Default'}]));
 
 const newTaskForm = document.getElementById('newTaskForm');
+const taskListBody = document.getElementById('taskListBody');
+const taskEditSpot
 
  function getTasksFromLocalStorage() {
-  
   return JSON.parse(localStorage.getItem('tasks'));
 }
 
+function saveTasksToLocalStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 function pushTaskToLocalStorage(task) {
-
   const existingTasks = getTasksFromLocalStorage();
-
   existingTasks.push(task);
-  // console.log(existingTasks);
   localStorage.setItem('tasks', JSON.stringify(existingTasks));
-  console.log(getTasksFromLocalStorage());
-
 }
-
-
-function createNewTask(event) {
-  event.preventDefault();
-  const taskName = event.target.inputTask.value;
-  const description = event.target.elements.inputDescription.value;
-  const dueDate = event.target.elements.inputDueDate.value;
-  let priority = '';
-    if (event.target.flexRadioDefault1.checked === true) {
-      priority = 'Low'
-    }
-    else if (event.target.flexRadioDefault2.checked === true) {
-     priority = 'Medium'
-    }
-    else if (event.target.flexRadioDefault3.checked === true) {
-      priority = 'High' 
-    } 
-  const category = event.target.category.value;
-  const taskNumber = Math.floor(Math.random() * 100);
-  const task = {'taskId': Date.now(), 'taskName': taskName,  'description': description, 'dueDate': dueDate,'priority': priority, 'category': category} 
-  pushTaskToLocalStorage(task)
-  
-}
-
-newTaskForm.addEventListener("submit", (event) => {
-  createNewTask(event);
-  });
 
 function getTaskById(taskId) {
   const tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -53,74 +26,111 @@ function getTaskById(taskId) {
   console.log(taskResult);
 }
 
-getTaskById(Math.floor(Math.random()*2699256952556));
+function deleteTask(taskId) {
+  const existingTasks = getTasksFromLocalStorage();
+  const indexOfTaskToDelete = existingTasks.findIndex(task => task.taskId === taskId);
+  if (indexOfTaskToDelete !== -1) {
+        existingTasks.splice(indexOfTaskToDelete, 1);
+    }
+saveTasksToLocalStorage(existingTasks);
+displayTaskList();
+}
+
+function createNewTask(event) {
+  event.preventDefault();
+    const taskName = event.target.inputTask.value;
+    const description = event.target.elements.inputDescription.value;
+    const dueDate = event.target.elements.inputDueDate.value;
+    let priority = '';
+      if (event.target.flexRadioDefault1.checked === true) {
+        priority = 'Low'
+      }
+      else if (event.target.flexRadioDefault2.checked === true) {
+        priority = 'Medium'
+      }
+      else if (event.target.flexRadioDefault3.checked === true) {
+        priority = 'High' 
+      } 
+    const category = event.target.category.value;
+    const taskNumber = Math.floor(Math.random() * 100);
+    const task = {'taskId': Date.now(), 'taskName': taskName,  'description': description, 'dueDate': dueDate,'priority': priority, 'category': category} 
+    pushTaskToLocalStorage(task);
+    displayTaskList();  
+}
 
 function createTaskListRow(task, taskId) {
   const row = document.createElement("tr");
 
   const idCell = document.createElement("th");
   idCell.textContent = task.taskId;
+  row.appendChild(idCell);
   const taskName = document.createElement("td");
   taskName.textContent = task.taskName;
+  row.appendChild(taskName);
   const description = document.createElement("td");
   description.textContent = task.description;
+  row.appendChild(description);
   const dueDate = document.createElement("td");
   dueDate.textContent = task.dueDate; 
+  row.appendChild(dueDate);
   const priority = document.createElement("td");
   priority.textContent = task.priority;
+  row.appendChild(priority);
   const category = document.createElement("td");
   category.textContent = task.category;
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("tableButton");
-  deleteButton.textContent = "Not up for it!";
-  deleteButton.addEventListener("click", () => deleteTask(index));
+  row.appendChild(category);
   const editButton = document.createElement("button");
-  eleteButton.classList.add("tableButton");
-  deleteButton.textContent = "Do it differently!";
-  deleteButton.addEventListener("click", () => editTask(index));
+    editButton.classList.add("btn", "btn-primary", "btn-sm");
+    editButton.textContent = "Do it differently!";
+    editButton.addEventListener("click", () => {
+        // TODO add the logic of editing a task
+    });
+
+    const editCell = document.createElement("td");
+    editCell.appendChild(editButton);
+    row.appendChild(editCell);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-danger",);
+    deleteButton.textContent = "Not up for it!";
+    deleteButton.addEventListener("click", () => {
+        deleteTask(task.taskId);
+        // displayTaskList();
+    });
+
+    const deleteCell = document.createElement("td");
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(deleteCell);
 
   return row;
 }
 
-function displayTaskList() {
-
-  taskListBody.innerHTML = "";
-
-  const taskId = Math.floor(Math.random()*2699256952556);
-  
-    getTaskById(taskId).forEach((task, taskId) => {
-      const taskRow = createTaskListRow(task, taskId);
-      taskListBody.appendChild(taskRow)
-    });  
-}
-
-displayTaskList();
-
-function deleteTask(taskId) {
-  getTaskById(taskId).splice(taskId,1);
-  displayTaskList();
-}
-
 function editTask(taskId) {
-  const oldTask = getTaskById(taskId);
+  const existingTasks = getTasksFromLocalStorage();
+  const oldTask = existingTasks.findIndex(task => task.taskId === taskId);
 
   const editFrom = document.createElement("form");
 
   const idCell = document.createElement("input");
   idCell.setAttribute("type", "text");
   idCell.textContent = oldTask.taskId;
+  editFrom.appendChild(idCell);
   const taskName = document.createElement("input");
   taskName.setAttribute("type", "text");
   taskName.textContent = oldTask.taskName;
+  editFrom.appendChild(taskName);
   const description = document.createElement("input");
   description.setAttribute("type", "text");
   description.textContent = oldTask.description;
+  editFrom.appendChild(description);
   const dueDate = document.createElement("input");
   dueDate.setAttribute("type", "date");
-  dueDate.textContent = oldTask.dueDate; 
+  dueDate.textContent = oldTask.dueDate;
+  editFrom.appendChild(dueDate);
   const priority = document.createElement("legend");
   priority.setAttribute("type", "text");
   priority.textContent = oldTask.priority;
+  editFrom.appendChild(priority);
     const priorityLow = document.createElement("input");
     priorityLow.setAttribute("type", "radio");
     const priorityMedium = document.createElement("input");
@@ -136,6 +146,7 @@ function editTask(taskId) {
   const category = document.createElement("select");
   category.setAttribute("type", "text");
   category.textContent = oldTask.category;
+  editFrom.appendChild(category)
     const categoryWork = document.createElement("option");
     categoryWork.setAttribute('value', 'Work');
     categoryWork.setAttribute('name', 'Work');
@@ -162,12 +173,35 @@ function editTask(taskId) {
 
 function displayTaskList() {
 
-  taskEditSpot.innerHTML = "";
+  taskListBody.innerHTML = "";
 
-  editTask()
-    ;  
+  const existingTasks = getTasksFromLocalStorage();
 
+    existingTasks.forEach(task => {
+      const taskRow = createTaskListRow(task);
+      taskListBody.appendChild(taskRow)
+    });  
 }
+
+
+newTaskForm.addEventListener("submit", (event) => {
+  createNewTask(event);
+  });
+
+
+displayTaskList();
+
+
+
+
+
+// function displayTaskList() {
+
+//   taskEditSpot.innerHTML = "";
+
+//   editTask();  
+
+// }
 // $('#myModal').on('shown.bs.modal', function () {
 //   $('#myInput').trigger('focus')
 // })
