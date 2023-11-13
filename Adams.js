d2846970bbc99ccb32e0078fed8f4ab85992e353
RikHenning <144657312+RikHenning@users.noreply.github.com>
2023-11-13 08:59:49 +0100

    13-11-2023
    Working on it. I'll get there

--- Adams.js
+++ Adams.js
@@ -1,10 +1,10 @@
 
-localStorage.clear();
-localStorage.setItem('tasks', JSON.stringify([{'taskId': 0, 'taskName': 'Default',  'description': 'Default', 'dueDate': 'Default','priority': 'Default', 'category': 'Default'}]));
+// localStorage.clear();
+// localStorage.setItem('tasks', JSON.stringify([{'taskId': 0, 'taskName': 'Default',  'description': 'Default', 'dueDate': 'Default','priority': 'Default', 'category': 'Default'}]));
 
 const newTaskForm = document.getElementById('newTaskForm');
 const taskListBody = document.getElementById('taskListBody');
-const taskEditSpot
+const taskEditSpot = document.getElementById('taskEditSpot');
 
  function getTasksFromLocalStorage() {
   return JSON.parse(localStorage.getItem('tasks'));
@@ -21,9 +21,9 @@ function pushTaskToLocalStorage(task) {
 }
 
 function getTaskById(taskId) {
-  const tasks = JSON.parse(localStorage.getItem('tasks'));
-  taskResult = tasks.filter(task => task.taskId === taskId)
-  console.log(taskResult);
+  const existingTasks = getTasksFromLocalStorage();
+  taskResult = existingTasks.filter(task => task.taskId === taskId);
+  return taskResult[0];
 }
 
 function deleteTask(taskId) {
@@ -31,9 +31,10 @@ function deleteTask(taskId) {
   const indexOfTaskToDelete = existingTasks.findIndex(task => task.taskId === taskId);
   if (indexOfTaskToDelete !== -1) {
         existingTasks.splice(indexOfTaskToDelete, 1);
+        saveTasksToLocalStorage(existingTasks);
+        displayTaskList();
     }
-saveTasksToLocalStorage(existingTasks);
-displayTaskList();
+
 }
 
 function createNewTask(event) {
@@ -79,13 +80,10 @@ function createTaskListRow(task, taskId) {
   const category = document.createElement("td");
   category.textContent = task.category;
   row.appendChild(category);
-  const editButton = document.createElement("button");
+  const editButton = document.createElement("a");
     editButton.classList.add("btn", "btn-primary", "btn-sm");
     editButton.textContent = "Do it differently!";
-    editButton.addEventListener("click", () => {
-        // TODO add the logic of editing a task
-    });
-
+    editButton.href = `edit.html?id=${task.taskId}`;
     const editCell = document.createElement("td");
     editCell.appendChild(editButton);
     row.appendChild(editCell);
@@ -105,71 +103,73 @@ function createTaskListRow(task, taskId) {
   return row;
 }
 
-function editTask(taskId) {
-  const existingTasks = getTasksFromLocalStorage();
-  const oldTask = existingTasks.findIndex(task => task.taskId === taskId);
+// function editTask(taskId) {
+//   const existingTasks = getTasksFromLocalStorage();
+//   const oldTask = existingTasks.findIndex(task => task.taskId === taskId);
 
-  const editFrom = document.createElement("form");
+//   const editFrom = document.createElement("form");
 
-  const idCell = document.createElement("input");
-  idCell.setAttribute("type", "text");
-  idCell.textContent = oldTask.taskId;
-  editFrom.appendChild(idCell);
-  const taskName = document.createElement("input");
-  taskName.setAttribute("type", "text");
-  taskName.textContent = oldTask.taskName;
-  editFrom.appendChild(taskName);
-  const description = document.createElement("input");
-  description.setAttribute("type", "text");
-  description.textContent = oldTask.description;
-  editFrom.appendChild(description);
-  const dueDate = document.createElement("input");
-  dueDate.setAttribute("type", "date");
-  dueDate.textContent = oldTask.dueDate;
-  editFrom.appendChild(dueDate);
-  const priority = document.createElement("legend");
-  priority.setAttribute("type", "text");
-  priority.textContent = oldTask.priority;
-  editFrom.appendChild(priority);
-    const priorityLow = document.createElement("input");
-    priorityLow.setAttribute("type", "radio");
-    const priorityMedium = document.createElement("input");
-    priorityMedium.setAttribute("type", "radio");
-    const priorityHigh = document.createElement("input");
-    priorityHigh.setAttribute("type", "radio");
-      if  (priority === "Low") {priorityLow.checked}
-       else if (priority === "Medium") {priorityMedium.checked}
-        else (priority === "High") {priorityHigh.checked}
-  priority.appendChild(priorityLow);
-  priority.appendChild(priorityMedium);
-  priority.appendChild(priorityHigh); 
-  const category = document.createElement("select");
-  category.setAttribute("type", "text");
-  category.textContent = oldTask.category;
-  editFrom.appendChild(category)
-    const categoryWork = document.createElement("option");
-    categoryWork.setAttribute('value', 'Work');
-    categoryWork.setAttribute('name', 'Work');
-    const categoryPersonal = document.createElement("option");
-    categoryPersonal.setAttribute('value', 'Personal');
-    categoryPersonal.setAttribute('name', 'Personal');
-    const categoryShopping = document.createElement("option");
-    categoryShopping.setAttribute('value', 'Shopping');
-    categoryShopping.setAttribute('name', 'Shopping');
-    const categoryGarden = document.createElement("option");
-    categoryGarden.setAttribute('value', 'Garden');
-    categoryGarden.setAttribute('name', 'Garden');
-     if (category === 'Work') {categoryWork.selected}
-      else if (category === 'Personal') {categoryPersonal.selected}
-        else if (category === 'Shopping') {categoryShopping.selected}
-          else (category === 'Garden') {categoryGarden.selected}
-  category.appendChild(categoryWork);
-  category.appendChild(categoryPersonal);
-  category.appendChild(categoryShopping);
-  category.appendChild(categoryGarden);
+//   const idCell = document.createElement("input");
+//   idCell.setAttribute("type", "text");
+//   idCell.textContent = oldTask.taskId;
+//   editFrom.appendChild(idCell);
+//   const taskName = document.createElement("input");
+//   taskName.setAttribute("type", "text");
+//   taskName.textContent = oldTask.taskName;
+//   editFrom.appendChild(taskName);
+//   const description = document.createElement("input");
+//   description.setAttribute("type", "text");
+//   description.textContent = oldTask.description;
+//   editFrom.appendChild(description);
+//   const dueDate = document.createElement("input");
+//   dueDate.setAttribute("type", "date");
+//   dueDate.textContent = oldTask.dueDate;
+//   editFrom.appendChild(dueDate);
+//   const priority = document.createElement("legend");
+//   priority.setAttribute("type", "text");
+//   priority.textContent = oldTask.priority;
+//   editFrom.appendChild(priority);
+//     const priorityLow = document.createElement("input");
+//     priorityLow.setAttribute("type", "radio");
+//     const priorityMedium = document.createElement("input");
+//     priorityMedium.setAttribute("type", "radio");
+//     const priorityHigh = document.createElement("input");
+//     priorityHigh.setAttribute("type", "radio");
+//       if  (priority === "Low") {priorityLow.checked}
+//        else if (priority === "Medium") {priorityMedium.checked}
+//         else (priority === "High") {priorityHigh.checked}
+//   priority.appendChild(priorityLow);
+//   priority.appendChild(priorityMedium);
+//   priority.appendChild(priorityHigh); 
+//   const category = document.createElement("select");
+//   category.setAttribute("type", "text");
+//   category.textContent = oldTask.category;
+//   editFrom.appendChild(category)
+//     const categoryWork = document.createElement("option");
+//     categoryWork.setAttribute('value', 'Work');
+//     categoryWork.setAttribute('name', 'Work');
+//     const categoryPersonal = document.createElement("option");
+//     categoryPersonal.setAttribute('value', 'Personal');
+//     categoryPersonal.setAttribute('name', 'Personal');
+//     const categoryShopping = document.createElement("option");
+//     categoryShopping.setAttribute('value', 'Shopping');
+//     categoryShopping.setAttribute('name', 'Shopping');
+//     const categoryGarden = document.createElement("option");
+//     categoryGarden.setAttribute('value', 'Garden');
+//     categoryGarden.setAttribute('name', 'Garden');
+//      if (category === 'Work') {categoryWork.selected}
+//       else if (category === 'Personal') {categoryPersonal.selected}
+//         else if (category === 'Shopping') {categoryShopping.selected}
+//           else (category === 'Garden') {categoryGarden.selected}
+//   category.appendChild(categoryWork);
+//   category.appendChild(categoryPersonal);
+//   category.appendChild(categoryShopping);
+//   category.appendChild(categoryGarden);
 
-  displayEditTask();
-}
+//   displayEditTask();
+// }
+
+if (newTaskForm) {
 
 function displayTaskList() {
 
@@ -190,6 +190,8 @@ newTaskForm.addEventListener("submit", (event) => {
 
 
 displayTaskList();
+
+}
 
 
 
